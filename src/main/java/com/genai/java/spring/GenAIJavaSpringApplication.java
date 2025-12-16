3774f127c98b31bdd80c0cae83f3ac114e00dd5f
ali gelenler <you@example.com>
2025-12-16 22:31:00 +0100

    section-3-lecture-3-chat-options

---
+++ src/main/java/com/genai/java/spring/GenAIJavaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.genai.java.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAIJavaSpringApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(GenAIJavaSpringApplication.class, args);
+    }
+}
---
+++ src/main/java/com/genai/java/spring/config/AIProviderConfig.java
@@ -0,0 +1,37 @@
+package com.genai.java.spring.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.huggingface.HuggingfaceChatModel;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AIProviderConfig {
+
+
+    @Bean("openAIChatClient")
+    ChatClient openAIChatClient(OpenAiChatModel openAiChatModel) {
+        return ChatClient.builder(openAiChatModel).build();
+    }
+
+    @Bean("vertexAIChatClient")
+    ChatClient vertexAIChatClient(VertexAiGeminiChatModel vertexAiGeminiChatModel) {
+        return ChatClient.builder(vertexAiGeminiChatModel).build();
+    }
+
+    @Bean("huggingFaceChatClient")
+    ChatClient huggingFaceChatClient(HuggingfaceChatModel huggingfaceChatModel) {
+        return ChatClient.builder(huggingfaceChatModel).build();
+    }
+
+    @Bean("ollamaChatClient")
+    ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel) {
+        return ChatClient.builder(ollamaChatModel).build();
+    }
+
+
+
+}
